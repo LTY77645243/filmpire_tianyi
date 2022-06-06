b5e5e385431e50f3490ae8c176648cc5056a3f66
LTY77645243 <49172166+LTY77645243@users.noreply.github.com>
2022-06-06 18:52:13 -0400

    sidebar component added

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,5 +1,7 @@
 import { makeStyles } from "@mui/styles";
 
+const drawerWidth = 240;
+
 export default makeStyles( (theme) => ({
     toolbar: {
         height: '80px',
@@ -11,10 +13,29 @@ export default makeStyles( (theme) => ({
             flexWrap: 'wrap',
         }
     },
+
     menuButton: {
         marginRight: theme.spacing(2),
         [theme.breakpoints.up('sm')]: {
             display: 'none',
         }
-    }
+    },
+
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+
+    drawerPaper: {
+        width:drawerWidth,
+    },
+
+    linkButton: {
+        '&:hover': {
+            color:'white !important',
+            textDecoration:'none',
+        },
+    },
 } ));
