c6b87a2ff50c4dae94b9717d8cd607236fc3f7b6
LTY77645243 <49172166+LTY77645243@users.noreply.github.com>
2022-06-07 14:29:11 -0400

    use redux slice to dispatch actions (click on genresor categories

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react';
 import { Divider,List,ListItem,ListItemText,ListSubheader,ListItemIcon,Box,CircularProgress } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import { useGetGenresQuery } from '../../services/TMDB';
 import useStyles from './styles';
 import genreIcons from '../../assets/genres';
@@ -26,6 +28,8 @@ const Sidebar = ({ setMobileOpen }) => {
     const theme = useTheme();
     const classes = useStyles();
     const { data, isFetching } = useGetGenresQuery();
+    const dispatch = useDispatch(); //* transfer data
+
 
     return (
         <>
@@ -41,7 +45,7 @@ const Sidebar = ({ setMobileOpen }) => {
                 <ListSubheader>Categories</ListSubheader>
                 { categories.map(({ label, value }) => (
                     <Link key={value} className={classes.links} to="/">
-                        <ListItem onClick={() => {}} button>
+                        <ListItem onClick={() => dispatch(selectGenreOrCategory(value))} button>
                             <ListItemIcon>
                                 <img src={genreIcons[label.toLowerCase()]} className={classes.genreImages} height={30} />
                             </ListItemIcon>
@@ -59,7 +63,7 @@ const Sidebar = ({ setMobileOpen }) => {
                     </Box>
                 ) : data.genres.map(({ name, id })=>(
                     <Link key={name} className={classes.links} to="/">
-                        <ListItem onClick={() => {}} button>
+                        <ListItem onClick={() => dispatch(selectGenreOrCategory(id))} button>
                             <ListItemIcon>
                                 <img src={genreIcons[name.toLowerCase()]} className={classes.genreImages} height={30} />
                             </ListItemIcon>
